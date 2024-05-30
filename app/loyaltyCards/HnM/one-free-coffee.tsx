a3c95ed5965454d09a230eb04a1d42cdcb75451b
adi-fullstack <adi@fullstack.ph>
2024-05-30 18:53:36 +0800

    jashdfkhasldf

--- app/loyaltyCards/HnM/one-free-coffee.tsx
+++ app/loyaltyCards/HnM/one-free-coffee.tsx
@@ -4,7 +4,7 @@ import { View, Text, Image, ScrollView } from 'react-native';
 const stampsData = [
   {
     id: 1,
-    stamps: 3, // Change this number to test different scenarios
+    stamps: 4, // Change this number to test different scenarios
   },
 ];
 
@@ -13,27 +13,31 @@ export default function Index() {
     <ScrollView>
       <View className="w-full p-4">
         {stampsData.map((card) => {
-          const totalBoxes = 10;
+          const totalBoxes = 9;
           const filledBoxes = card.stamps;
           const emptyBoxes = totalBoxes - filledBoxes;
           
           return (
-            <View key={card.id} className="flex flex-row flex-wrap border-b-2 border-gray-200 pb-2 mb-2 w-full">
+            <View key={card.id} className="flex flex-row flex-wrap content-start justify-around gap-y-8 w-full">
               {/* Render filled stamp boxes */}
               {Array.from({ length: filledBoxes }).map((_, index) => (
-                <View key={index} className="w-1/3 p-2">
+                <View key={index} className="p-8 w-5/12 h-auto border border-solid border-gray-300">
                   <Image
-                    className="w-full h-auto"
+                    className="w-auto h-auto m-auto"
                     source={require('../../../assets/images/stamp.png')}
                   />
                 </View>
               ))}
               {/* Render empty boxes with numbers */}
               {Array.from({ length: emptyBoxes }).map((_, index) => (
-                <View key={filledBoxes + index} className="w-1/3 p-2 flex items-center justify-center border border-solid border-gray-600">
-                  <Text className="text-lg font-bold">{filledBoxes + index + 1}</Text>
+                <View key={filledBoxes + index} className="flex items-center p-8 w-5/12 h-auto border border-solid border-gray-300">
+                  <Text className="text-8xl font-bold text-gray-400 mx-auto my-0">{filledBoxes + index + 1}</Text>
                 </View>
               ))}
+
+                <View className="flex items-center content-center p-8 w-5/12 h-auto border border-solid border-gray-300">
+                  <Text className="h-auto text-4xl font-bold text-lime-500 m-auto uppercase">Free</Text>
+                </View>
             </View>
           );
         })}
